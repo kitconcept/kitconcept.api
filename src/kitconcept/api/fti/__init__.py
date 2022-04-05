fa3250353115b0b36ac24b9a58977e1f3cf963a0
Victor Fernandez de Alba <sneridagh@gmail.com>
2022-04-05 16:01:31 +0200

    Format

--- src/kitconcept/api/fti/__init__.py
+++ src/kitconcept/api/fti/__init__.py
@@ -1,9 +1,9 @@
 """Implement an api to manage fti in Plone."""
+from ._fti import add_behavior_for_type
 from ._fti import behaviors_for_type
 from ._fti import get
 from ._fti import get_behavior_registration
 from ._fti import get_types
-from ._fti import add_behavior_for_type
 from ._fti import remove_behavior_for_type
 
 
